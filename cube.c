e61ccc34052c6961fff7a9941d2853e14170c2a3
lucietessy <123073199+lucietessy@users.noreply.github.com>
2023-03-01 23:38:47 +0300

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,12 @@
+//program to find cube up to a geven interger
+#include <stdio.h.>
+int main ()
+{
+    int x=1;
+    do
+    {printf("%d\n",x);
+    x=x+1;
+    printf("number is %d and cube of the %d is is,\n",x,x,(x*x*x));
+    while(x<=3);
+    return 0;
+}
